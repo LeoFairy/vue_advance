c826634db1f18421d3ed259da6f7f7381ad0ad42
ysf <1142669510@qq.com>
2024-11-19 10:41:30 +0800

    mixin混入(合)

---
+++ src/components/MySchool.vue
@@ -0,0 +1,27 @@
+<template>
+  <div >
+    <h2 @click="showName"> 学校名称：{{name}}</h2>
+    <h2> 学校地址： {{address}}</h2>
+    </div>
+</template>
+
+<script>
+  //引入mixin
+  // import{hun,hun2} from '../mixin'
+
+  export default {
+    name:'MySchool',
+    data () {
+      return {
+      name:'vue',
+      address:'北京',
+      x:666
+      }
+    },
+    // mixins: [hun,hun2],
+    mounted() {
+      console.log("你好啊!!!!!!!");
+    },
+  }
+</script>
+
---
+++ src/mixin.js
@@ -0,0 +1,19 @@
+export const hun = {
+  methods: {
+    showName() {
+      alert(this.name);
+    },
+  },
+  mounted() {
+    console.log("你好啊");
+  },
+};
+
+export const hun2 = {
+  data() {
+    return {
+      x: 100,
+      y: 200,
+    };
+  },
+};
