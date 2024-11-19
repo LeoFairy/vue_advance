46796b91af47eb64012ec45689db6c04dccfa99e
ysf <1142669510@qq.com>
2024-11-19 10:12:21 +0800

    配置项props

---
+++ src/components/MyStudent.vue
@@ -0,0 +1,56 @@
+<template>
+  <div >
+    <h1>{{ msg }}</h1>
+    <h2> 学生姓名：{{name}}</h2>
+    <h2> 学生性别： {{sex}}</h2>
+    <h2> 学生年龄： {{myAge+1}}</h2>
+    <button @click="updateAge">尝试修改收到的年龄</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'MySchool',
+  data () {
+    return {
+      msg:'我是一个学生',
+      myAge:this.age
+    }
+  },
+  methods:{
+    updateAge(){
+      this.myAge++
+    }
+  },
+  // props:['name','sex','age'] 简单接收
+
+  // 接受同时，对类型进行限制
+/*   props:{
+    name:String,
+    age:Number,
+    sex:String
+  } */
+
+    // 接受同时，对类型进行限制+ 默认值和必要值的指定
+  props:{
+    name:{
+      type:String,
+      required:true
+    },
+    age:{
+      type:Number,
+      default:99
+    },
+    sex:{
+      type:String,
+      required:true
+    }
+  }
+}
+</script>
+
+<style>
+ .school{
+  background-color: gray;
+ }
+</style>
