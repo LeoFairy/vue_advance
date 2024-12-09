91ed0d75160bf7a189b1e4a59e26a4ad83ff1402
ysf <1142669510@qq.com>
2024-12-09 16:28:24 +0800

    history模式与hash模式

---
+++ 40_src_history模式与hash模式/pages/About.vue
@@ -0,0 +1,30 @@
+<template>
+  <h2>我是About的内容</h2>
+</template>
+
+<script>
+  export default {
+    name:'About',
+    //通过路由规则，进入该组件时被调用
+    beforeRouteEnter (to, from, next) {
+      console.log('app-beforeRouteEnter',to,from);
+            // if (to.path === "/home/news" || to.path === "home/message") {
+            //判断是否需要授权
+            if (to.meta.isAuth) {
+              if (localStorage.getItem("study") === "vue") {
+                next();
+              } else {
+                alert("学习项目不对，无法查看");
+              }
+            } else {
+              next();
+            }
+    },
+    //通过路由规则，离开该组件时被调用
+    beforeRouteLeave (to, from, next) {
+      console.log('app-beforeRouteLeave',to,from);
+      next();
+    }
+
+  }
+</script>
